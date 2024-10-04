90e21756df6d0c91052d85f08fb483190901e1fc
muralidharan <muralidharan@MAVERIC>
2024-10-05 00:18:18 +0530

    Add project files.

---
+++ InstituteManagement.API/Data/InstituteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InstituteManagement.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstituteManagement.API.Data
+{
+    public class InstituteDbContext : DbContext
+    {
+        public InstituteDbContext(DbContextOptions<InstituteDbContext> dbContextOptions) : base(dbContextOptions) 
+        {
+
+        }
+
+        DbSet<Student> Students { get; set; }   
+    }
+}
